7e524f73ee9a821b73bd75efbccdefa000f70123
rayjwjwjwejjew <aa18258237411@gmail.com>
2026-04-02 21:59:13 +0800

    Update Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+var _a;
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 var __filename = fileURLToPath(import.meta.url);
 var __dirname = path.dirname(__filename);
+var repoName = (_a = process.env.GITHUB_REPOSITORY) === null || _a === void 0 ? void 0 : _a.split('/')[1];
+var base = process.env.GITHUB_ACTIONS && repoName ? "/".concat(repoName, "/") : '/';
 export default defineConfig({
+    base: base,
     plugins: [react()],
     resolve: {
         alias: {
